848e0d40bc080c50d352482eaa977e390929d150
Amina Belabbes <amina.belabbes@medionmail.com>
2020-08-17 21:49:09 +0200

    First Commit

---
+++ src/components/Me.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <v-button></v-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Me extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<style scoped lang="scss"></style>
