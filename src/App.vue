7225784680eadf8fff8f9fc2e130d30abfcd48df
Amina Belabbes <amina.belabbes@medionmail.com>
2020-08-17 23:24:05 +0200

    first array push, colors implemented

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <v-app>
-    
-
     <v-main>
       <!-- <HelloWorld /> -->
       <Me />
@@ -28,8 +26,76 @@ export default Vue.extend({
 
 <style lang="scss">
 
+  *, ::before, ::after {
+    box-sizing: border-box;
+  }
+
   .body {
     font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
-    color: 
+    color: #292528;
+    padding: 30px;
   }
+
+  .primary {
+    color: #F9A03D;
+  }
+
+  .primary-bg {
+    background-color: #F9A03D;
+  }
+
+   .primary-light {
+    color: #FAB94C;
+  }
+
+  .primary-light-bg {
+    background-color: #FAB94C;
+  }
+
+  .secondary-cl {
+    color: #3EC5FB;
+  }
+
+  .secondary-cl-bg {
+    background-color: #3EC5FB;
+  }
+
+   .secondary-light {
+    color: #ADE6FF;
+  }
+
+  .secondary-light-bg {
+    background-color: #ADE6FF;
+  }
+
+  .alert {
+    color: #D64550;
+  }
+
+  .alert-bg {
+    background-color: #D64550;
+  }
+
+  .success {
+    color: #44AF69;
+  }
+
+  .success-bg {
+    background-color: #44AF69;
+  }
+
+  .brown {
+    color: #90583C;
+  }
+
+  .brown-bg {
+    background-color: #90583C;
+  }
+
+  .white-cl {
+    color: white;
+  }
+
+
+
 </style>
--- src/components/Me.vue
+++ src/components/Me.vue
@@ -2,15 +2,26 @@
   <div>
     <div class="grid-container">
       <div class="profile-data">
-        <h2>Profile</h2>
+        <h2><div class="roundedImage"></div>Profile</h2>
+        <br/>
         <span class="inline-group"><h4>Name: </h4> <div>{{name}}</div> </span>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-
-         <v-btn color="green" @click="remindOfSuccess()">Success!</v-btn>
+        <span class="inline-group"><h4>Klasse: </h4> <div>{{heroClass}}</div> </span>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+        <br/>
+        <v-btn color="green" @click="remindOfSuccess()">Success!</v-btn>
       </div>
-      <div class="quest-data">quest data, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged..</div>
-      <div class="side-quest-data">Side quest data,  It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</div>  
-      <div class="item4"><img src="./../assets/warriorprincess.jpg" height="100%" width="330px"></div>
+      <div class="quest-data primary-light-bg"><h4>Your Quests:</h4>
+      <ol>
+        <li v-for="questItem in quests" :key="questItem.title"> 
+          <h5>{{questItem.title}}</h5>
+          <p>{{questItem.description}} </p> 
+          <div>{{questItem.dueDate}}</div> 
+        </li>
+      </ol>
+      
+      </div>
+      <div class="side-quest-data secondary-cl-bg white-cl">Side quest data,  It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</div>  
+      <div class="hero-image secondary-light-bg"><img src="./../assets/warriorprincess.jpg"  height="580px" width="330px"></div>
       <div class="item5">5</div>
       <div class="item6">6</div>
       <div class="item7">7</div>
@@ -27,9 +38,13 @@ export default class Me extends Vue {
   // @Prop() private msg!: string;
   levelCount: number = 1;
   name: string = 'Amina';
+  heroClass: string = 'Wallküre';
+  quest: Object = { title: 'Finish my first jira ticket', description: 'work out what is missing', dueDate: '1 week', party: 'Blueend', fellows: 'Stefan'}; 
+  quests: Object[] = [];
 
   remindOfSuccess() {
-    this.levelCount = this.levelCount++;
+    this.levelCount++;
+    this.quests.push(this.quest);
     console.log("Level count", this.levelCount)
     alert("Wow, you went up 1 level! That's now at Level " + this.levelCount + "!")
   }
@@ -40,24 +55,30 @@ export default class Me extends Vue {
 
   .grid-container {
     display: grid;
-    grid-gap: 10px;
+    grid-gap: 20px;
     grid-template-columns: 1fr 2fr;
     grid-template-rows: 2fr 0.5fr 0.5fr 1fr 2fr;
   }
 
   .profile-data {
     grid-area: 1 / 1 / span 1 / span 1;
-    background-color: lightcoral;
+    padding: 20px 25px 20px 25px;
   }
  
   .quest-data {
     grid-area: 2 / 1 / span 1 / span 1;
-    background-color: purple;
+    padding: 20px 25px 15px 15px;
   }
 
   .side-quest-data {
     grid-area: 3 / 1 / span 1 / span 1;
-    background-color: greenyellow;
+    padding: 20px 25px 15px 15px;
+  }
+  .hero-image {
+     grid-area: 1 / 2 / span 3 / span 1;
+     display: flex;
+     flex-direction: column;
+     justify-content: center;
   }
 
   .inline-group {
@@ -68,6 +89,19 @@ export default class Me extends Vue {
     div {
       margin-left: 10px;
     }
+  }
+
+  .roundedImage {
+    background: url('./../assets/warriorqueen.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    
+    overflow:hidden;
+    -webkit-border-radius:50px;
+    -moz-border-radius:50px;
+    border-radius:50px;
+    width:90px;
+    height:90px;
   }
   
   /**
